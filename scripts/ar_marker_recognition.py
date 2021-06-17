9bb87d85f19f3c0c1776d2bb8fd8283b2c34fdb9
nakalab <nakalab@example.com>
2021-06-17 18:22:46 +0900

    マーカーの姿勢計算追加

--- scripts/ar_marker_recognition.py
+++ scripts/ar_marker_recognition.py
@@ -8,6 +8,7 @@ import numpy as np
 import tf
 import yaml
 import os
+import math
  
  
 # 物体情報のpublisher
@@ -19,13 +20,14 @@ def ilist(lst): return [ int(i) for i in lst ]
 def flist(lst): return [ float(i) for i in lst ]
 
 # 物体情報を送信
-def send_objects_info(rects, positions, labels):
+def send_objects_info(rects, positions, labels, quaternions):
     global pub_objinfo
     br = tf.TransformBroadcaster()
     object_info = []
     for i, p in enumerate(positions):
         br.sendTransform(p,
-                tf.transformations.quaternion_from_euler(0, 0, 0),
+                #tf.transformations.quaternion_from_euler(0, 0, 0),
+                quaternions[i],
                 rospy.Time.now(), "ar:%d"%(labels[i]), "camera_depth_optical_frame")
         object_info.append(
             { 
@@ -51,6 +53,9 @@ def pointcloud_cb( pc2 ):
     rects = []
     positions = []
     labels = []
+    quaternions = []
+    img_display = np.copy( img )
+
     for i in range(N):
         lt = np.min( corners[i][0], 0 )
         rb = np.max( corners[i][0], 0 )
@@ -60,14 +65,37 @@ def pointcloud_cb( pc2 ):
         rects.append( (lt, rb) )
         positions.append( xyz[ int(cy), int(cx) ] )
         labels.append( ids[i] )
-    
-    img_display = np.copy( img )
-    cv2.aruco.drawDetectedMarkers(img_display, corners, ids, (0,255,0))
 
+        # 姿勢（クオータニオン）を計算
+        p1 = xyz[ int(corners[i][0][0][1]), int(corners[i][0][0][0]) ]
+        p2 = xyz[ int(corners[i][0][1][1]), int(corners[i][0][1][0]) ]
+        p3 = xyz[ int(corners[i][0][3][1]), int(corners[i][0][3][0]) ]
+        
+        # z軸回転とy軸回転でx軸を一致させる回転を計算
+        v = p2 - p1
+        r = math.atan2( v[1], v[0] )
+        q = tf.transformations.quaternion_about_axis(r, [0,0,1])
+
+        r = -math.atan2( v[2], v[0] )
+        q = tf.transformations.quaternion_multiply( q, tf.transformations.quaternion_about_axis(r, [0,1,0]) )
+
+        # x軸回転でy軸を一致させる回転を計算
+        v = p3 - p1
+        r = math.atan2( v[2], v[1] )
+        q = tf.transformations.quaternion_multiply( q, tf.transformations.quaternion_about_axis(r, [1,0,0]) )
+
+        quaternions.append( q )
+
+        # 計算使った点を描画
+        cv2.circle( img_display, tuple(ilist(corners[i][0][0])), 10, (255,0,0) )
+        cv2.circle( img_display, tuple(ilist(corners[i][0][1])), 10, (0,255,0) )
+        cv2.circle( img_display, tuple(ilist(corners[i][0][3])), 10, (0,0,255) )
+
+    cv2.aruco.drawDetectedMarkers(img_display, corners, ids, (0,255,0))
     cv2.imshow('img',img_display)
     cv2.waitKey(10)
     
-    send_objects_info( rects, positions, labels )
+    send_objects_info( rects, positions, labels, quaternions )
 
 
 def main():
