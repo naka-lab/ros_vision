5918c734fdd43d71816dbf5161f343c27d04d458
nakatomo <nakalab@example.com>
2022-11-09 17:31:35 +0900

    openpose修正

--- scripts/open_pose.py
+++ scripts/open_pose.py
@@ -13,6 +13,7 @@ import gdown
 import torch
 from open_pose_utils.decode_pose import decode_pose
 from open_pose_utils.openpose_net import OpenPoseNet
+import queue
 
 os.chdir( os.path.abspath(os.path.dirname(__file__)) )
 
@@ -69,12 +70,7 @@ def send_maker( x, y, z, id, scale=0.05, rgb=(1,0,0) ):
     pub_marker.publish(marker_data)
 
 
-def pointcloud_cb( pc2 ):
-    lag = rospy.get_time()-pc2.header.stamp.secs
-    if lag>0.5:
-        print("discard queue")
-        return 
-
+def proc_pointcloud( pc2 ):
     xyz = np.frombuffer(pc2.data, dtype=np.float32).reshape(pc2.height, pc2.width, 8)[:,:,0:3]
     img = np.frombuffer(pc2.data, dtype=np.uint8).reshape(pc2.height, pc2.width, 32)[:, :,16:19]
 
@@ -222,8 +218,24 @@ def estimate_pose( ori_img ):
 
 def main():
     load_model()
+    #rospy.Subscriber("/camera/depth_registered/points", PointCloud2, pointcloud_cb, queue_size=1)
+    #rospy.spin()
+
+    que = queue.Queue()
+
+    def pointcloud_cb( pc2 ):
+        if que.qsize()>0:
+            try:
+                que.get_nowait()
+            except queue.Empty:
+                pass
+        que.put(pc2)
+
     rospy.Subscriber("/camera/depth_registered/points", PointCloud2, pointcloud_cb, queue_size=1)
-    rospy.spin()
+
+    while not rospy.is_shutdown():
+        proc_pointcloud( que.get() ) 
+
 
 
 if __name__ == '__main__':
